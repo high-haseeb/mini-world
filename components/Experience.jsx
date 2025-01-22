541ed0c11064829829760e0c61d1e2fde9aaaf60
high-haseeb <haseebkhalidoriginal@gmail.com>
2025-01-22 10:02:02 +0500

    added some lighting

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,22 +1,29 @@
 "use client";
-import { OrbitControls, Stats } from '@react-three/drei';
+import { Environment, OrbitControls, Stats } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import World from './World';
 
 const Experience = () => {
     return (
         <div className="w-full h-full">
-            <Canvas className="w-full h-full" camera={{ position: [0, 0, -7] }} >
+            <Canvas className="w-full h-full" camera={{ position: [0, 0, -7] }} shadows>
                 <Stats />
                 <ambientLight intensity={1.0} />
                 <directionalLight
                     color="white"
-                    position={[0, 15, -10]}
+                    position={[0, -15, -10]}
                     castShadow
                     shadow-mapSize-width={2048}
                     shadow-mapSize-height={2048}
                 />
-                <OrbitControls autoRotate/>
+
+                {/* <ambientLight intensity={0.3} /> */}
+                <directionalLight position={[5, 5, 5]} intensity={1} castShadow />
+                <pointLight position={[-5, 5, -5]} intensity={1} />
+                <spotLight position={[0, 5, 0]} angle={0.15} intensity={1} castShadow />
+
+                <Environment preset='city' environmentIntensity={0.4} />
+                <OrbitControls autoRotate />
                 <World />
             </Canvas>
         </div>
